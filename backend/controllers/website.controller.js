6b2590a4ce56431524727819167bf2289d6a5507
Sanchita Behera <160879256+224ASTHA@users.noreply.github.com>
2026-04-01 00:21:28 +0530

    Update website.controller.js

--- backend/controllers/website.controller.js
+++ backend/controllers/website.controller.js
@@ -340,8 +340,7 @@ const deploy = async(req, res) => {
 const getBySlug = async(req, res) => {
     try{
         const website = await Website.findOne({
-            slug: req.params.slug,
-            user: req.user._id
+            slug: req.params.slug
         })
         if(!website){
             return res.status(400).json({ message: "website not found" })
