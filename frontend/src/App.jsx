cce09dd1ae3e1d5d8f7cdfa74a4ebf0b619353a7
Sanchita Behera <160879256+224ASTHA@users.noreply.github.com>
2026-03-31 23:34:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import WebsiteEditor from "./pages/Editor";
 import LiveSite from "./pages/LiveSite";
 import Pricing from "./pages/Pricing";
 
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://generatewebsite.onrender.com"
 
 function App() {
   const {userData} = useSelector(state=>state.user)
