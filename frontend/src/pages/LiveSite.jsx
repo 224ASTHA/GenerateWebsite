0ee5d46c40910fdd2c0dc854b9008568fe0131ca
Sanchita Behera <160879256+224ASTHA@users.noreply.github.com>
2026-04-01 00:19:01 +0530

    Update LiveSite.jsx

--- frontend/src/pages/LiveSite.jsx
+++ frontend/src/pages/LiveSite.jsx
@@ -11,8 +11,7 @@ const LiveSite = () => {
   useEffect(() => {
       const handleGetWebsite = async () => {
           try{
-              const result = await axios.get(`${serverUrl}/api/website/get-by-slug/${id}`,
-                  {withCredentials: true}
+              const result = await axios.get(`${serverUrl}/api/website/get-by-slug/${id}`
               )
               setHtml(result.data.latestCode)
           }catch(error){
